02eca884644b8f27204f1e9d40c1f31ad43cfc30
Möslinger Markus, ANECON <m.moeslinger@viennaairport.com>
2015-06-01 13:15:22 +0200

    - more tests

--- src/test/java/org/outofrange/steht/StateMachineTest.java
+++ src/test/java/org/outofrange/steht/StateMachineTest.java
@@ -3,15 +3,51 @@ package org.outofrange.steht;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.outofrange.steht.StateMachineTest.TestStates.*;
 
 public class StateMachineTest {
 	private StateMachineBuilder<TestStates> b;
+	private List<Object> register;
 
 	@Before
 	public void prepare() {
 		b = StateMachineBuilder.with(TestStates.class);
+		register = new ArrayList<>();
+	}
+
+	@Test
+	public void actionOfOneTransitionIsExecuted() {
+		b.from(A).to(B, () -> register.add(1)).startAt(A).go(B);
+
+		assertTrue(register.contains(1));
+	}
+
+	@Test
+	public void twoActionsOfOneTransitionIsExecuted() {
+		b.from(A).to(B, () -> register.add(1)).to(B, () -> register.add(2)).startAt(A).go(B);
+
+		assertTrue(register.indexOf(1) == 0);
+		assertTrue(register.indexOf(2) == 1);
+	}
+
+	@Test
+	public void actionsOnShortestPathAreExecuted() {
+		b.from(A).to(B, () -> register.add(1)).to(B, () -> register.add(2))
+				.from(B).to(C, () -> register.add(3)).from(C).to(D).from(D).to(E, () -> register.add(4))
+				.from(A).to(F, () -> register.add(9))
+				.startAt(A).go(E);
+
+		assertTrue(register.indexOf(1) == 0);
+		assertTrue(register.indexOf(2) == 1);
+		assertTrue(register.indexOf(3) == 2);
+		assertTrue(register.indexOf(4) == 3);
+		assertFalse(register.contains(9));
 	}
 
 	@Test
@@ -44,6 +80,52 @@ public class StateMachineTest {
 
 		assertEquals(fsm.getCurrentState(), A);
 		assertEquals(fsm.getTransitionsDone(), 0);
+	}
+
+	@Test
+	public void resetWorksAndChangesTransitionCounter() {
+		StateMachine<TestStates> fsm = b.from(A).to(B).from(A).to(C).startAt(A);
+
+		assertEquals(fsm.getCurrentState(), A);
+		assertEquals(fsm.getTransitionsDone(), 0);
+
+		fsm.go(B);
+		assertEquals(fsm.getCurrentState(), B);
+		assertEquals(fsm.getTransitionsDone(), 1);
+
+		fsm.reset();
+		fsm.go(C);
+		assertEquals(fsm.getCurrentState(), C);
+		assertEquals(fsm.getTransitionsDone(), 1);
+	}
+
+	@Test
+	public void setStateDoesntChangeTransitionCounter() {
+		StateMachine<TestStates> fsm = b.from(A).to(B).from(A).to(C).startAt(A);
+
+		assertEquals(fsm.getCurrentState(), A);
+		assertEquals(fsm.getTransitionsDone(), 0);
+
+		fsm.go(B);
+		assertEquals(fsm.getCurrentState(), B);
+		assertEquals(fsm.getTransitionsDone(), 1);
+
+		fsm.setState(A);
+		fsm.go(C);
+		assertEquals(fsm.getCurrentState(), C);
+		assertEquals(fsm.getTransitionsDone(), 2);
+	}
+
+	@Test
+	public void creatingWithExplicitStates() {
+		StateMachineBuilder<TestStates> builder = StateMachineBuilder.with(new TestStates[]{A, B, C});
+
+		assertGoWorks(builder.from(A).to(B).from(B).to(C), A, C, 2);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void exceptionWhenConfiguringNonValidState() {
+		StateMachineBuilder.with(new TestStates[]{A, B, C}).from(D).to(E);
 	}
 
 	@Test
